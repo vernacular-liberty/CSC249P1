01427ea5c93286618466f258b21e1bc7b6d37c7c
vernacular-liberty <112956848+vernacular-liberty@users.noreply.github.com>
2023-09-29 09:35:24 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
-welcomeMessage = "Welcome to the Equilateral Triangle calculator"
+welcomeMessage = "Welcome to the Regular Shape calculator"
 
 print("Client starting - connecting to server at IP", HOST, "and port", PORT)
 
@@ -13,6 +13,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))#establish connection
     print(f"Connection established, sending message: '{welcomeMessage}'")
     s.sendall(bytes(welcomeMessage, 'utf-8'))#send message
+
+    #Enter number of sides
+    sides = 1
+    while sides < 2 or sides > 8:    
+        sides = int(input("Enter the number of sides between 3-7: "))
+    s.sendall(str(sides).encode())
 
     #Enter side length and send
     side_length = float(input("Enter the length of the sides: "))
@@ -26,8 +32,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(operation.encode())
     print("Messages sent, waiting for reply")
 
+    data = s.recv(1024)  # Receive the result first
+    shape = s.recv(1024)  # Then receive the shape
 
-    data = s.recv(1024)#get data
+print(f"Received server data response: '{data!r}' [{len(data)} bytes]")
+print(f"Received server shape response: '{shape!r}' [{len(shape)} bytes]")
 
-print(f"Received response: '{data!r}' [{len(data)} bytes]")
-print("Client is done!")
+if operation == 'a':
+    print(f"The area of a {shape} with {sides} sides is {data} units squared.")
+elif operation == 'p':
+    print(f"The perimeter of a {shape} with {sides} sides is {data} units.")
+
--- server.py
+++ server.py
@@ -4,11 +4,13 @@ import math
 HOST = "127.0.0.1"
 PORT = 65432
 
+#establish server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     print("Server starting - listening for connections at IP", HOST, "and port", PORT)
-
+    
+    #search for connections
     conn, addr = s.accept()
     with conn:
         print(f"Connected established with {addr}")
@@ -18,6 +20,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not welcomeMessage:
                 break
             print(f"Received client message: '{welcomeMessage.decode()}' [{len(welcomeMessage)} bytes]")
+
+            sides = conn.recv(1024)
+            if not sides:
+                break
+            print(f"Received client sides amount: '{sides!r}' [{len(sides)} bytes]")
+            sides = int(sides)
 
             side_length = conn.recv(1024)
             if not side_length:
@@ -32,12 +40,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             operation = operation.decode()
 
             if operation == "a":
-                result = (math.sqrt(3) / 4) * side_length**2
+                if sides == 3:
+                    result = (math.sqrt(3) / 4) * side_length**2
+                    shape = "triangle"
+                elif sides == 4:
+                    result = side_length**2
+                    shape = "square"
+                elif sides == 5:
+                    result = (1/4) * (5 * (5 + 2 * math.sqrt(5))) * side_length**2
+                    shape = "pentagon"
+                elif sides == 6:
+                    result = ((3 * math.sqrt(3))/2) * side_length**2
+                    shape == "hexagon"
+                elif sides == 7:
+                    result = (7/4) * side_length**2 * (1 / math.tan(math.pi/7))
+                    shape = "heptagon"
+                else:
+                    result = "This shape is not available yet."
             elif operation == "p":
-                result = 3 * side_length
+                result = sides * side_length
             else:
-                result = "Invalid choice"
+                result = "Invalid operation choice"
 
             conn.sendall(str(result).encode())
+            conn.sendall(shape.encode())
             
 print("Server is done!")
